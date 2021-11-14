22d2837b18ca7ae8cc4cadea7e1a038c0e9dd8bc
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-11-13 23:37:25 -0500

    feat: added the functionality to make go boom

---
+++ src/IPlugin.cs
@@ -0,0 +1,11 @@
+﻿namespace PlugableAddin
+{
+    public interface IPlugin
+    {
+        void Execute();
+
+        void Activate(Inventor.Application inventorApplication, string ClientId, bool firstTime = true);
+
+        void Deactivate();
+    }
+}
