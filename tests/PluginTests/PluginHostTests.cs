b0daebc6f5b0cc4b96683e506bc88b4873f31c4d
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-11-21 23:41:45 -0500

    tests: creation

---
+++ tests/PluginTests/PluginHostTests.cs
@@ -0,0 +1,32 @@
+﻿using InventorCode.Plugin;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace PluginTests
+{
+    [TestFixture]
+    public class PluginHostTests
+    {
+        [Test]
+        public void PluginHostConstructor_ActivatesPlugin()
+        {
+            var host = new PluginHost();
+            host.ComposePlugins();
+
+            Action test = () => host.ActivateAll(null, "test");
+
+            Assert.Throws<NotImplementedException>(() => test());
+        }
+
+        [Test]
+        public void PluginHost_FindsTwoInternalPlugins()
+        {
+            var host = new PluginHost();
+            host.ComposePlugins();
+            var plugins = host.Plugins;
+
+            Assert.AreEqual(2, plugins.Count());
+        }
+    }
+}
---
+++ tests/PluginTests/TestPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventorCode.Plugin;
+using Inventor;
+using System.ComponentModel.Composition;
+
+namespace PluginTests
+{
+    [Export(typeof(IPlugin))]
+    internal class TestPlugin : IPlugin
+    {
+
+        public void Activate(global::Inventor.Application inventorApplication, string ClientId, bool firstTime = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
