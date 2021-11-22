663d16d932f7dea59c8c8188f86bd5b08f873f3a
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-11-21 23:44:28 -0500

    demo: add an Embedded Plugin Demo

---
+++ demo/PluginHostDemo/EmbeddedPluginDemo.cs
@@ -0,0 +1,29 @@
+﻿using InventorCode.Plugin;
+using System.ComponentModel.Composition;
+using System.Windows.Forms;
+
+namespace PluginHostDemo
+{
+    [Export(typeof(IPlugin))]
+    internal class EmbeddedPluginDemo : IPlugin
+    {
+        private Inventor.Application _inventorApplication;
+        private string _clientId;
+
+        public void Activate(Inventor.Application InventorApplication, string ClientId, bool firstTime = true)
+        {
+            _inventorApplication = InventorApplication;
+            _clientId = ClientId;
+            MessageBox.Show("EmbeddedPluginDemo Loaded.");
+        }
+
+        public void Deactivate()
+        {
+            _inventorApplication = null;
+        }
+
+        public void Execute()
+        {
+        }
+    }
+}
